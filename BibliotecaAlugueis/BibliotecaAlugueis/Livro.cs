9df4bab9ed57e7586a66125119370596968fba4c
mgbatista <maiara.27bueno@gmail.com>
2021-03-14 10:42:46 -0300

    Adicionando classe Livro

---
+++ BibliotecaAlugueis/BibliotecaAlugueis/Livro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaAlugueis
+{
+    public class Livro
+    {
+        public long NumeroTombo { get; set; }
+        public string ISBN { get; set; }
+        public string Titulo { get; set; }
+        public string Genero { get; set; }
+        public DateTime DataPublicacao { get; set; }
+        public string Autor { get; set; }
+
+
+        public override string ToString()
+        {
+            return ("Número Tombo: " + NumeroTombo + "\nISBN: " + ISBN + "\nTítulo: " + Titulo + "Gênero: " + Genero + "Data de Publicação: " + DataPublicacao + "Autor: " + Autor);
+        }
+    }
+}
