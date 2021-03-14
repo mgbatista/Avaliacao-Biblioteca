f1f0dcd7e1df291647eacb494fc72c8024494fb9
mgbatista <maiara.27bueno@gmail.com>
2021-03-14 10:25:57 -0300

    initial commit

---
+++ BibliotecaAlugueis/BibliotecaAlugueis/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaAlugueis
+{
+    class Cliente
+    {
+        public long IdCliente { get; set; }
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Telefone { get; set; }
+
+
+        public override string ToString()
+        {
+            return ("Id: " + IdCliente + "\nCPF: " + Cpf + "\nNome: " + Nome + "\nData de Nascimento: " + DataNascimento + "\nTelefone: " + Telefone);
+        }
+
+    }
+}
