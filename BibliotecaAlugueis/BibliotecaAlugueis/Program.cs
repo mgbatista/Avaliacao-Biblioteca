4e624be301d297db5fae77e01a0d071adab5d77b
mgbatista <maiara.27bueno@gmail.com>
2021-03-14 10:52:58 -0300

    Criando menu

--- BibliotecaAlugueis/BibliotecaAlugueis/Program.cs
+++ BibliotecaAlugueis/BibliotecaAlugueis/Program.cs
@@ -10,6 +10,30 @@ namespace BibliotecaAlugueis
     {
         static void Main(string[] args)
         {
+            Cliente c = new Cliente();
+            Endereco e = new Endereco();
+            Livro l = new Livro();
+
+            int escolhaMenu = 0;
+
+            do
+            {
+                Console.WriteLine("\n*** BIBLIOTECA MUNICIPAL ***");
+                Console.WriteLine("\nMENU");
+                Console.WriteLine("\n1- Cadastro de Cliente");
+                Console.WriteLine("2- Cadastro de Livro");
+                Console.WriteLine("3- Empréstimo de Livro");
+                Console.WriteLine("4- Devolução de Livro");
+                Console.WriteLine("5- Relatório de Empréstimos e Devoluções");
+
+                Console.Write("\nInforme a opção desejada: ");
+                escolhaMenu = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("");
+
+            } while (escolhaMenu <=0 && escolhaMenu >= 6);
+                  
+
         }
     }
 }
